47a839b77f17e8df5b180020ec504dfc1645f8b5
serralva-ruben <91135825+serralva-ruben@users.noreply.github.com>
2023-05-12 00:00:27 +0000

    launcher file added

---
+++ launcher.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def launch_server():
+    subprocess.Popen(["python3", "client/main_client.py"])
+
+def launch_client():
+    subprocess.Popen(["python3", "server/main_server.py"])
+
+if __name__ == "__main__":
+    launch_server()
+    launch_client()
